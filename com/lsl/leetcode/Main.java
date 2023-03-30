9a5e42b59638cdf84cb685d20d67121d172d226d
lishulong <1323184983@qq.com>
2023-03-30 14:12:01 +0800

    牛客、Leetcode 刷题

--- com/lsl/leetcode/Main.java
+++ com/lsl/leetcode/Main.java
@@ -1,5 +1,8 @@
 package com.lsl.leetcode;
 
+import java.util.Scanner;
+import java.util.concurrent.SynchronousQueue;
+
 /**
  * @author : lishulong
  * @date : 11:36  2023/3/6
@@ -9,7 +12,129 @@ package com.lsl.leetcode;
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int m=3,n=7;
-        System.out.println(solution.uniquePaths(m, n));
+        int []nums = new int[]{7,7,7,7,7,7,7};
+        System.out.println(solution.lengthOfLIS(nums));
     }
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        String str = sc.nextLine();
+//        int n = str.length();
+//        int res = 0;
+//        for (int i = 1,j=0; i < n;) {
+//            if(str.charAt(i)==str.charAt(j)){
+//                res += 1;
+//                i += 2;
+//                j += 2;
+//            }else {
+//                i += 1;
+//                j += 1;
+//            }
+//        }
+//        System.out.println(res);
+//    }
+
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        int m = sc.nextInt();
+//        int k = sc.nextInt();
+//        int i = 0,j;
+//        char[][] colors = new char[n][m];
+//        int[][] nums = new int[n][m];
+//        int[][] res = new int[n][m];
+//        int temp = n;
+//        int temp1, temp2;
+//        while (temp>0){
+//            String str = sc.next();
+//            colors[i] = str.toCharArray();
+//            temp--;
+//            i++;
+//        }
+//        for (i = 0; i < n; i++) {
+//            for (j = 0; j < m; j++) {
+//                nums[i][j] = sc.nextInt();
+//            }
+//        }
+//        res[0][0] = nums[0][0];
+//        int maxValue = 0;
+//        for (i = 1; i < n; i++) {
+//            if(res[i-1][0] < 0){
+//                res[i][0] = -1;
+//                continue;
+//            }
+//            if(colors[i-1][0] != colors[i][0]){
+//                res[i][0] = res[i-1][0] - k < 0 ? -1 : res[i-1][0] - k + nums[i][0];
+//            }else{
+//                res[i][0] = res[i-1][0] + nums[i][0];
+//            }
+//            if(res[i][0]>maxValue){
+//                maxValue = res[i][0];
+//            }
+//        }
+//        for (j = 1; j < m; j++) {
+//            if(res[0][j-1] < 0){
+//                res[0][j] = -1;
+//                continue;
+//            }
+//            if(colors[0][j] != colors[0][j-1]){
+//                res[0][j] = res[0][j-1] - k < 0 ? -1 : res[0][j-1] - k + nums[0][j];
+//            }else{
+//                res[0][j] = res[0][j-1] + nums[0][j];
+//            }
+//            if(res[0][j]>maxValue){
+//                maxValue = res[0][j];
+//            }
+//        }
+//
+//        for (i = 1; i < n; i++) {
+//            for (j = 1; j < m; j++) {
+//                if (res[i - 1][j] < 0 && res[i][j - 1] < 0) {
+//                    res[i][j] = -1;
+//                    continue;
+//                }
+//                temp1 = colors[i - 1][j] != colors[i][j] ? res[i - 1][j] - k : res[i - 1][j];
+//                temp2 = colors[i][j - 1] != colors[i][j] ? res[i][j - 1] - k : res[i][j - 1];
+//                res[i][j] = Math.max(temp1, temp2) < 0 ? -1 : Math.max(temp1, temp2) + nums[i][j];
+//                if (res[i][j] > maxValue) {
+//                    maxValue = res[i][j];
+//                }
+//            }
+//        }
+//        System.out.println(maxValue);
+//    }
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        int[] s = new int[n];
+//        int[] t = new int[n];
+//        int maxTime = 0;
+//        int minTime = Integer.MAX_VALUE;
+//        for (int i = 0; i < n; i++) {
+//            s[i] = sc.nextInt();
+//            if(minTime > s[i]){
+//                minTime = s[i];
+//            }
+//        }
+//        for (int i = 0; i < n; i++) {
+//            t[i] = sc.nextInt();
+//            if(maxTime < t[i]){
+//                maxTime = t[i];
+//            }
+//        }
+//        int[] nums = new int[maxTime - minTime + 1];
+//        int maxCount = Integer.MIN_VALUE;
+//        int timeCount = 0;
+//        for (int i = 0; i < n; i++) {
+//            for (int j = s[i]; j <= t[i]; j++) {
+//                nums[j-minTime] += 1;
+//                if(nums[j-s[i]] > maxCount){
+//                    maxCount = nums[j-s[i]];
+//                    timeCount = 1;
+//                }else if(nums[j-s[i]] == maxCount){
+//                    timeCount += 1;
+//                }
+//            }
+//        }
+//        System.out.printf("%d %d\n",maxCount,timeCount);
+//    }
 }
--- com/lsl/leetcode/Solution.java
+++ com/lsl/leetcode/Solution.java
@@ -1,5 +1,9 @@
 package com.lsl.leetcode;
 
+import com.sun.management.GcInfo;
+
+import java.util.Arrays;
+
 /**
  * @author : lishulong
  * @date : 11:29  2023/3/6
@@ -151,5 +155,253 @@ public class Solution {
         }
         return false;
     }
+
+    /**
+     * 312. 戳气球
+     * @param nums
+     * @return
+     */
+    public int[][] rec;
+    public int[] val;
+    public int maxCoins(int[] nums) {
+        int n = nums.length;
+        val = new int[n+2];
+        for (int i = 1; i <= n; i++) {
+            val[i] = nums[i-1];
+        }
+        val[0] = val[n+1] = 1;
+        rec = new int[n+2][n+2];
+        for (int i = 0; i <= n+1; i++) {
+            Arrays.fill(rec[i],-1);
+        }
+        return solve(0,n+1);
+    }
+
+    public int solve(int left,int right){
+        if(left >= right - 1){
+            return 0;
+        }
+        if(rec[left][right] != -1){
+            return rec[left][right];
+        }
+        for (int i = left+1; i < right; i++) {
+            int sum = val[left] * val[i] * val[right];
+            sum += solve(left,i) + solve(i,right);
+            rec[left][right] = Math.max(rec[left][right], sum);
+        }
+        return rec[left][right];
+    }
+
+
+    public int maxCoins_dp(int[] nums) {
+        int n = nums.length;
+        int[][] res = new int[n+2][n+2];
+        int[] val = new int[n+2];
+        val[0] = val[n+1] = 1;
+        for (int i = 1; i <= n; i++) {
+            val[i] = nums[i-1];
+        }
+        for (int i = n-1; i >=0 ; i--) {
+            for (int j = i+2; j <= n+1 ; j++) {
+                for (int k = i+1; k < j; k++) {
+                    int sum = val[i] * val[k] * val[j];
+                    sum += res[i][k] + res[k][j];
+                    res[i][j] = Math.max(res[i][j],sum);
+                }
+            }
+        }
+        return res[0][n+1];
+    }
+
+    public int maxCoins_dp_2(int[] nums) {
+        int n = nums.length;
+        int[][] res = new int[n+2][n+2];
+        int[] val = new int[n+2];
+        val[0] = val[n+1] = 1;
+        for (int i = 1; i <= n; i++) {
+            val[i] = nums[i-1];
+        }
+        for (int i = 2; i <= n+1; i++) {
+            for (int j = i - 2; j >= 0; j--) {
+                for (int k = j + 1; k < i; k++) {
+                    int sum = val[i] * val[k] * val[j];
+                    sum += res[i][k] + res[k][j];
+                    res[i][j] = Math.max(res[i][j],sum);
+                }
+            }
+        }
+        return res[n+1][0];
+    }
+
+    /**
+     * 72. 编辑距离
+     * @param word1
+     * @param word2
+     * @return
+     */
+    public int minDistance(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+        if(n*m == 0){
+            return n+m;
+        }
+        int[][] dp = new int[n+1][m+1];
+        for (int i = 0; i < n+1; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j < m+1; j++) {
+            dp[0][j] = j;
+        }
+        for (int i = 1; i < n+1; i++) {
+            for (int j = 1; j < m+1; j++) {
+                int left = dp[i-1][j]+1;
+                int down = dp[i][j-1]+1;
+                int left_down = dp[i-1][j-1];
+                if(word1.charAt(i-1)!=word2.charAt(j-1)){
+                    left_down += 1;
+                }
+                dp[i][j] = Math.min(left,Math.min(down,left_down));
+            }
+        }
+        return dp[n][m];
+    }
+
+
+    static final int MOD = 1000000007;
+    public int numDecodings(String s){
+        int n = s.length();
+        long a=0,b=1,c=0;
+        for (int i = 1; i <= n; i++) {
+            c = b * check1digit(s.charAt(i-1)) % MOD;
+            if(i>1){
+                c = (c + a * check2digits(s.charAt(i-2),s.charAt(i-1))) % MOD;
+            }
+            a = b;
+            b = c;
+        }
+        return (int) c;
+    }
+
+    public int check1digit(char ch) {
+        if (ch == '0') {
+            return 0;
+        }
+        return ch == '*' ? 9 : 1;
+    }
+
+    public int check2digits(char c0, char c1) {
+        if (c0 == '*' && c1 == '*') {
+            return 15;
+        }
+        if (c0 == '*') {
+            return c1 <= '6' ? 2 : 1;
+        }
+        if (c1 == '*') {
+            if (c0 == '1') {
+                return 9;
+            }
+            if (c0 == '2') {
+                return 6;
+            }
+            return 0;
+        }
+        return (c0 != '0' && (c0 - '0') * 10 + (c1 - '0') <= 26) ? 1 : 0;
+    }
+
+    /**
+     *
+     * @param nums
+     * @return
+     */
+    public boolean canPartition(int[] nums) {
+        int n = nums.length;
+        if(n<2){
+            return false;
+        }
+        int sum = 0,maxNum = 0;
+        for (int num : nums) {
+            sum = sum + num;
+            maxNum = Math.max(num,maxNum);
+        }
+        if(sum % 2 != 0){
+            return false;
+        }
+        int target = sum / 2;
+        if(maxNum > target){
+            return false;
+        }
+        boolean [][]dp = new boolean[n][target+1];
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = true;
+        }
+        dp[0][nums[0]] = true;
+        for (int i = 1; i < n; i++) {
+            int num = nums[i];
+            for (int j = 1; j <= target; j++) {
+                if(j>=num){
+                    dp[i][j] = dp[i-1][j]|dp[i-1][j-num];
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[n-1][target];
+    }
+
+    /**
+     * 给定一个整数数组，如果可以将数组划分为两个子集，使得两个子集中的元素之和相等返回true,反之 false
+     * @param nums
+     * @return
+     */
+    public boolean canPartition_improvement(int[] nums) {
+        int n = nums.length;
+        if(n<2){
+            return false;
+        }
+        int sum = 0,maxNum = 0;
+        for (int num : nums) {
+            sum = sum + num;
+            maxNum = Math.max(num,maxNum);
+        }
+        if(sum % 2 != 0){
+            return false;
+        }
+        int target = sum / 2;
+        if(maxNum > target){
+            return false;
+        }
+
+        boolean []dp = new boolean[target+1];
+        dp[0]=true;
+        for (int num : nums) {
+            for (int i=target;i>=num;i--){
+                dp[i] = dp[i] | dp[i-num];
+            }
+        }
+        return dp[target];
+    }
+
+
+    /**
+     * 给定一个整数数组，如果可以将数组划分为两个子集，使得两个子集中的元素之和相等返回true,反之 false
+     * @param nums
+     * @return
+     */
+    public int lengthOfLIS(int[] nums) {
+        int []dp = new int[nums.length];
+        dp[0] = 1;
+        int maxans = 1;
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if(nums[i] > nums[j]){
+                    dp[i] = Math.max(dp[i],dp[j]+1);
+                }
+            }
+            maxans = Math.max(maxans, dp[i]);
+        }
+        return maxans;
+    }
+
 
 }
